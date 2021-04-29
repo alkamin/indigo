264786e6415ca0e4bf2621f601eac186f8b14b8e
Alex Kaminsky <akaminsky@azavea.com>
2021-04-29 10:00:10 -0400

    feat: small theme customization example

--- src/templates/src/theme.ts
+++ src/templates/src/theme.ts
@@ -1,6 +1,10 @@
 import { extendTheme } from "@chakra-ui/react";
 import { ChakraTheme } from "@chakra-ui/theme";
 
-const theme: ChakraTheme = extendTheme({});
+const theme: ChakraTheme = extendTheme({
+  colors: {
+    primary: "lightGray",
+  },
+});
 
 export default theme;
