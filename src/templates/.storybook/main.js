9dfc9b598ba660b7be71c8d45c6fc475adf83bf0
Alex Kaminsky <akaminsky@azavea.com>
2021-08-06 11:14:48 -0400

    feat: include storybook a11y plugin by default

--- src/templates/.storybook/main.js
+++ src/templates/.storybook/main.js
@@ -11,7 +11,11 @@ module.exports = {
     "../stories/**/*.stories.@(js|jsx|ts|tsx)",
     "../stories/**/stories.@(js|jsx|ts|tsx)",
   ],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-a11y",
+  ],
   typescript: {
     check: false,
     checkOptions: {},
