5fe7110d31b5850786c86b92d8a9025e483e30b7
Alex Kaminsky <akaminsky@azavea.com>
2021-08-17 14:30:28 -0400

    Finish generate command

--- src/lib/generate-component.ts
+++ src/lib/generate-component.ts
@@ -5,6 +5,7 @@ import * as copyfiles from "copyfiles";
 import * as glob from "glob";
 import * as fs from "fs";
 import * as mv from "mv";
+import * as findUp from "find-up";
 
 type UserInput = {
   name: string;
@@ -13,9 +14,27 @@ type UserInput = {
 
 const generate = async (userInput: UserInput): Promise<void> =>
   new Promise((resolve, reject) => {
-    // TODO: verify there is a package.json file with the indigo flag
+    const packageFilePath = findUp.sync("package.json");
 
-    const targetDir = `./src/components/${userInput.name}`;
+    if (!packageFilePath) {
+      throw new Error("A package.json file could not be found and is required");
+    }
+
+    const packageContents = JSON.parse(
+      fs.readFileSync(packageFilePath).toString()
+    );
+
+    const isCreatedByIndigo = packageContents.indigo;
+
+    if (!isCreatedByIndigo) {
+      throw new Error(
+        "This package doesn't seem to have been created with Indigo"
+      );
+    }
+
+    const basePath = path.dirname(packageFilePath);
+
+    const targetDir = `${basePath}/src/components/${userInput.name}`;
 
     const tmpDir = tmp.dirSync({
       unsafeCleanup: true,
